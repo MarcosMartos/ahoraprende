6df6789ec0a524c645019888153e85f240aa5eb0
Marcos <marcosmartostuvi@gmail.com>
2026-07-09 16:41:21 -0300

    imagenes por imageskit.io

--- src/data/products.ts
+++ src/data/products.ts
@@ -14,45 +14,45 @@ export const productosDestacados: Producto[] = [
     titulo: "JS 538",
     precio: 850000,
     categoria: "Auriculares",
-    imagen: "../public/js538_auris.webp",
+    imagen: "https://ik.imagekit.io/puaijw6o8/js538_auris.webp",
     stock: 2,
-    marca: "js380"
+    marca: "js380",
   },
   {
     id: "2",
     titulo: "Jbl Wave 380",
     precio: 45000,
     categoria: "Auriculares",
-    imagen: "../public/jbl_wave380.webp",
+    imagen: "https://ik.imagekit.io/puaijw6o8/jbl_wave380_1.webp",
     stock: 15,
-    marca: "jbl"
+    marca: "jbl",
   },
   {
     id: "3",
     titulo: "Parlante Go 3",
     precio: 38000,
     categoria: "Parlantes",
-    imagen: "../public/jbl_go3_parlante.webp",
+    imagen: "https://ik.imagekit.io/puaijw6o8/jbl_go3_parlante.webp",
     stock: 5,
-    marca: "jbl"
+    marca: "jbl",
   },
   {
     id: "4",
     titulo: "Microfono corbatero x2",
     precio: 1200000,
     categoria: "Microfonos",
-    imagen: "../public/furry_soul_microfonos.webp",
+    imagen: "https://ik.imagekit.io/puaijw6o8/furry_soul_microfonos.webp",
     stock: 1,
-    marca: "Soul"
+    marca: "Soul",
   },
   {
     id: "5",
     titulo: "Redmi Buds 6 Play",
     precio: 1200000,
     categoria: "Auriculares",
-    imagen: "../public/redmi_buds6_auris.webp",
+    imagen: "https://ik.imagekit.io/puaijw6o8/redmi_buds6_auris.webp",
     stock: 1,
-    marca: "Redmi"
-  }
+    marca: "Redmi",
+  },
   // Puedes repetir o agregar los demás aquí...
 ];
